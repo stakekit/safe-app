c7d1a256c780972f60f4b7999d0717100f362b2d
Petar Todorovic <petar@omni.app>
2025-04-08 14:29:46 +0200

    feat: theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,30 +2,25 @@ import "../styles/global.css";
 import { EB_Garamond, Roboto_Flex } from "next/font/google";
 import { layoutContainer } from "./style.css";
 
-export const metadata = {
-	title: "StakeKit",
-	description: "StakeKit",
-};
-
 const roboto = Roboto_Flex({
-	subsets: ["latin"],
-	variable: "--sk-font-roboto",
+  subsets: ["latin"],
+  variable: "--sk-font-roboto",
 });
 const ebGaramond = EB_Garamond({
-	subsets: ["latin"],
-	variable: "--sk-font-garamond",
-	weight: "400",
+  subsets: ["latin"],
+  variable: "--sk-font-garamond",
+  weight: "400",
 });
 
 export default function RootLayout({
-	children,
+  children,
 }: {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-	return (
-		<html lang="en" className={`${roboto.variable} ${ebGaramond.variable}`}>
-			<link rel="icon" href="/icon.png" />
-			<body className={layoutContainer}>{children}</body>
-		</html>
-	);
+  return (
+    <html lang="en" className={`${roboto.variable} ${ebGaramond.variable}`}>
+      <link rel="icon" href="/icon.png" />
+      <body className={layoutContainer}>{children}</body>
+    </html>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,18 @@
 import React from "react";
 import { Box } from "../components/atoms/box";
-import { FluidContainer } from "./fluid";
 import { Header } from "./header";
 import { container } from "./style.css";
 import { Widget } from "./widget";
 import { HelpModals } from "./widget/help-modals";
 
 export default function Home() {
-	return (
-		<Box className={container}>
-			<FluidContainer />
-			<Header />
-			<Widget />
-			<Box marginTop={{ tablet: "0", mobile: "8" }}>
-				<HelpModals />
-			</Box>
-		</Box>
-	);
+  return (
+    <Box className={container}>
+      <Header />
+      <Widget />
+      <Box marginTop={{ tablet: "0", mobile: "8" }}>
+        <HelpModals />
+      </Box>
+    </Box>
+  );
 }
